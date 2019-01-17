946874d713f1ddbcade1e142709e7c8afb956173
zhao.feng <zhao.feng@Ctrip.com>
2019-01-17 13:45:51 +0800

    增加设备信息 实现tabbar

--- FMDemo/Classes/BaseModule/Views/FMTabBar.m
+++ FMDemo/Classes/BaseModule/Views/FMTabBar.m
@@ -7,10 +7,76 @@
 //
 
 #import "FMTabBar.h"
+#import "FMMidPlayView.h"
+#import "UIView+FMLayout.h"
+#import "FMDeviceInfo.h"
 
 
+inline CGSize ScreenSize()
+{
+    CGSize size = [UIScreen mainScreen].bounds.size;
+    
+    return size;
+}
+
+
+@interface FMTabBar ()
+@property(nonatomic, weak)FMMidPlayView *middlePlayView;
+@end
 
 @implementation FMTabBar
 
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (!self) return nil;
+    
+    
+    return self;
+}
+
+- (void)stepUI{
+    
+    self.barStyle = UIBarStyleBlack;
+    
+    self.backgroundImage = [UIImage imageNamed:@"tabbar_bg"];
+    
+    self.middlePlayView.centerX = self.centerX;
+    
+    if ([FMDeviceInfo isIphoneX]) {
+        self.middlePlayView.y = ScreenSize().height - self.middlePlayView.height - 10;
+    }else{
+        self.middlePlayView.y = ScreenSize().height - self.middlePlayView.height;
+    }
+}
+
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+    
+    NSUInteger itemCount = self.items.count;
+    CGFloat btnW = self.width / (itemCount + 1);
+    NSInteger index = 0;
+    for (UIView *subView in self.subviews) {
+        
+        if ([subView isKindOfClass:NSClassFromString(@"UITabBarButton")]) {
+            //tabbar button 有4个原生的 中间一个自定义的 原生第三个移动到自定义后面
+            if (index == itemCount / 2) {
+                index ++;
+            }
+            CGFloat btnX = index * btnW;
+        }
+    }
+}
+
+- (FMMidPlayView *)middlePlayView
+{
+    if (!_middlePlayView) {
+        FMMidPlayView *midPlayView = [FMMidPlayView shareInstance];
+        [self addSubview:midPlayView];
+        _middlePlayView = midPlayView;
+    }
+    return _middlePlayView;
+}
 
 @end
---
+++ FMDemo/Classes/FunctionModule/Utils/FMDeviceInfo.h
@@ -0,0 +1,19 @@
+//
+//  FMDeviceInfo.h
+//  FMDemo
+//
+//  Created by zhao.feng on 2019/1/17.
+//  Copyright © 2019年 zf. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FMDeviceInfo : NSObject
+
++ (BOOL)isIphoneX
+
+@end
+
+NS_ASSUME_NONNULL_END
